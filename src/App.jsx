e8ecb22283ae7e94c74f22e2b52f31001312e409
Enrique <ejsv1899@gmail.com>
2024-05-27 18:43:12 -0600

    ejercicio 5.2 completado

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,16 @@ const App = () => {
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const blogs = await blogService.getAll();
-  //     setBlogs(blogs);
-  //   };
-
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      setUsername(user.username);
+      blogService.setToken(user.token);
+      getData();
+    }
+  }, []);
 
   const getData = async () => {
     const blogs = await blogService.getAll();
@@ -39,6 +41,7 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user); //get token
       setPassword("");
@@ -47,6 +50,11 @@ const App = () => {
       setPassword("");
       console.log("Wrong credentials");
     }
+  };
+
+  const logOut = () => {
+    window.localStorage.removeItem("loggedUser");
+    setUser(null);
   };
 
   return (
@@ -67,7 +75,8 @@ const App = () => {
         <>
           <h2>blogs</h2>
 
-          {`${username} logged in`}
+          {`${username} logged in `}
+          <button onClick={logOut}>Logout</button>
 
           <br />
           <br />
