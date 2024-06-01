97419f74883ada73d5af5008aa0bda9ca8600605
Enrique <ejsv1899@gmail.com>
2024-06-01 15:42:15 -0600

    ejercicio 5.7 completado

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const hidden = <>
+  {blog.title} by {blog.author}{" "}
+  <button onClick={toggleVisibility}>view</button>
+  </>
+
+  const notHidden = <>
+  {blog.title} by {blog.author}{" "}
+  <button onClick={toggleVisibility}>hide</button>
+  <br />
+  {blog.url}
+  <br />
+  likes: {blog.likes} <button>like</button>
+  <br />
+  {blog.author}
+  </>
+
+  return (
+    <div className="blog">
+      {visible ? notHidden : hidden}
+    </div>
+  );
+};
+
+export default Blog;
